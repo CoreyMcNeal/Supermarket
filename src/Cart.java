2ddc156d7a3ca2bb2f7944e2e0e5ea109fc9c3ca
Corey McNeal <mcnealcorey@icloud.com>
2021-08-10 18:26:39 -0700

    Added grabItem()

--- src/Cart.java
+++ src/Cart.java
@@ -15,21 +15,29 @@ public class Cart {
 
     public void addToCart() {
         while (true){
+            System.out.println("**Enter blank to finish**");
             System.out.print("Pick: ");
             String itemName = reader.nextLine();
-            if (!(validateName(itemName))) {
+            if (!(validateName(itemName))) {                //loops if item name isn't found in store stock
                 continue;
             }
 
             System.out.print("Amount: ");
+
+            int amount = 0;
             try {
-                int amount = Integer.valueOf(reader.nextLine());
+                amount = Integer.valueOf(reader.nextLine());
             } catch (Exception e) {
                 System.out.println("Enter an amount please.");
                 continue;
             }
 
+            if (!(validateAmount(itemName, amount))) {      // loops back if stock isn't available
+                continue;
+            }
 
+            grabItem(itemName, amount);
+            return;
         }
     }
 
@@ -39,6 +47,23 @@ public class Cart {
 
     public void setItemsInCart(Map<Item,Integer> itemsInCart) {
         this.itemsInCart = itemsInCart;
+    }
+
+    public void grabItem(String itemName, int amount) {
+        Item chosenItem = null;
+        for (Item entry: this.store.getAllShelves().keySet()) {
+            if (entry.getItemName().equals(itemName)) {
+                chosenItem = entry;
+            }
+        }
+
+        int newStock = this.store.getAllShelves().get(chosenItem) - amount;
+
+        this.store.getAllShelves().put(chosenItem, newStock);
+        this.itemsInCart.put(chosenItem, amount);
+
+        System.out.println(this.itemsInCart);
+        System.out.println(this.store.getAllShelves());
     }
 
     public boolean validateName(String name) {
@@ -52,7 +77,20 @@ public class Cart {
     }
 
     public boolean validateAmount(String name, int amount) {
-        return true; //temp
+        Item product = null;
+        for (Item entry: this.store.getAllShelves().keySet()) {
+            if (entry.getItemName().equals(name)) {
+                product = entry;
+            }
+        }
+
+        if (this.store.getAllShelves().get(product) < amount) {
+            System.out.println("Not enough in stock.\n");
+            return false;
+        }
+
+        return true;
+        // eventually add the product and amount to shopping cart and also take it away from the stores map in addToCart()
     }
 
 }
